d8ae1f64ad17db3ffaaa5bb19bff11b56943c7cc
Abiola Adebanjo <abiola.adebanjo@andela.com>
2019-06-06 19:16:46 +0100

    Updates based on lesson 2: details of what I learnt in readme

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,18 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(
     MaterialApp(
-      home: Center(
-        child: Text("Hello "),
+      home: Scaffold(
+        backgroundColor: Colors.blueGrey,
+        appBar: AppBar(
+          title: Text('I Am Rich'),
+          backgroundColor: Colors.blueGrey[900],
+        ),
+        body: Center(
+          child: Image(
+            image: NetworkImage(
+                'https://upload.wikimedia.org/wikipedia/commons/5/58/Sunset_2007-1.jpg'),
+          ),
+        ),
       ),
     ),
   );
